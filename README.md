51b0b57026323e1759b3a2ddca399a94b5b63a56
Paul Davis <paulcdavis135@gmail.com>
2023-11-11 15:34:32 -0500

    First Commit with ES6 Starter Code

---
+++ README.md
@@ -0,0 +1,43 @@
+# Starter code provided by [Battery Acid Dev](https://www.youtube.com/@BatteryAcidDev)
+
+## Modifications We Made
+- Updating JavaScript code to modern ES6 import statements + async/awawit syntax
+- Updating to use JavaScript AWS SDK V3 instead of V2 (client.send paradigm)
+
+
+# Original Project README from Battery Acid Dev
+
+## Unity Realtime GameLift 2022 - Lambda Companion Project 
+
+An AWS Lambda project that handles all of the major game session actions for the project.
+
+## Main Project
+
+* https://github.com/BatteryAcid/unity-realtime-gamelift-2022
+
+## Tutorial Video  
+
+* https://youtu.be/rw0SkOTo31E
+    
+## 👋 Let's talk shop 👇  
+[Discord] https://discord.gg/psjbBDvNBK  
+[Twitter] https://twitter.com/BatteryAcidDev
+
+## Resources
+
+❗️❗️ Required Configuration ❗️❗️  
+[Lambda - GameLift Region] https://github.com/BatteryAcid/unity-realtime-gl-lambda-2022/blob/master/index.js#L5  
+[Lambda - GameLift Queue]  https://github.com/BatteryAcid/unity-realtime-gl-lambda-2022/blob/master/index.js#L6  
+
+[Resources]
+[Get started with Realtime servers] https://docs.aws.amazon.com/gamelift/latest/developerguide/realtime-plan.html   
+[Setup GameLift client] https://docs.aws.amazon.com/gamelift/latest/developerguide/gamelift-sdk-client-api.html  
+[Special Considerations AWS SDK Unity Support] https://docs.aws.amazon.com/sdk-for-net/v3/developer-guide/unity-special.html  
+[GameLift Realtime Client SDK] https://aws.amazon.com/gamelift/getting-started/  
+[GameLift service API reference (AWS SDK)] https://docs.aws.amazon.com/gamelift/latest/developerguide/reference-awssdk.html  
+[Setting up queues for game session placement] https://docs.aws.amazon.com/gamelift/latest/developerguide/queues-intro.html  
+[New AWS re:Post for GameLift questions] https://repost.aws/tags/TAF8-XUqojTsadH5jSz3IfGQ/amazon-game-lift  
+[Complex Realtime script setup answers] https://forums.awsgametech.com/t/realtime-server-project-setup/6607  
+ 
+
+#gametech #gamedev #awscommunitybuilder #unity
---
+++ index.js
@@ -0,0 +1,232 @@
+// Created by @BatteryAcidDev
+// This is the script that we upload to Lambda
+
+// const { v4: uuidv4 } = require('uuid');
+import { v4 as uuidv4 } from "uuid";
+import {
+    GameLiftClient,
+    SearchGameSessionsCommand,
+    DescribeGameSessionQueuesCommand,
+    StartGameSessionPlacementCommand,
+    CreatePlayerSessionCommand
+} from "@aws-sdk/client-gamelift";
+const gameLiftClient = new GameLiftClient({ region: 'us-east-1' });
+const TARGET_GAMELIFT_QUEUE_NAME = "rtgl-queue-2022-1";
+const REQUEST_FIND_MATCH = "1";
+const MAX_PLAYER_COUNT = 2; // This can be updated to suit your game's requirements
+
+async function searchGameSessions(targetAliasARN) {
+    let gameSessionFilterExpression = "hasAvailablePlayerSessions=true";
+
+    let searchGameSessionsRequest = {
+        AliasId: targetAliasARN,
+        FilterExpression: gameSessionFilterExpression,
+        SortExpression: "creationTimeMillis ASC"
+    }
+
+    const command = new SearchGameSessionsCommand(searchGameSessionsRequest);
+    try {
+        const data = await gameLiftClient.send(command);
+
+        if (data.GameSessions && data.GameSessions.length > 0) {
+            console.log("We have game sessions");
+            return data.GameSessions[0]
+        }
+        else {
+            console.log("No game sessions");
+            return null;
+        }
+    }
+    catch (error) {
+        console.log(error);
+        return null;
+    }
+
+    // return await gameLiftClient.send(command).promise().then(data => {
+    //     // console.log(data);
+
+    //     if (data.GameSessions && data.GameSessions.length > 0) {
+    //         console.log("We have game sessions");
+    //         return data.GameSessions[0]
+    //     }
+    //     else {
+    //         console.log("No game sessions");
+    //         return null;
+    //     }
+    // }).catch(err => {
+    //     console.log(err);
+    //     return null;
+    // });
+}
+
+async function getActiveQueue() {
+    let options = {
+        "Limit": 5 // how many GameLift queues to return
+    }
+    
+    const command = new DescribeGameSessionQueuesCommand(options);
+    try {
+        const data = await gameLiftClient.send(command);
+
+        if (data.GameSessionQueues && data.GameSessionQueues.length > 0) {
+            // for now just grab the first Queue
+            console.log("We have some queues");
+            
+            // if multiple queues, need to refactor this
+            let selectedGameSessionQueue;
+            data.GameSessionQueues.forEach(gameSessionQueue => {
+                if (gameSessionQueue.Name == TARGET_GAMELIFT_QUEUE_NAME) {
+                    selectedGameSessionQueue = gameSessionQueue;
+                }
+            });
+            return selectedGameSessionQueue;
+        }
+        else {
+            console.log("No queues available");
+            return [];
+        }
+    }
+    catch (error) {
+        console.log(error);
+        return [];
+    }
+
+    // return await gameLiftClient.send(command).promise().then(data => {
+    //     // console.log(data);
+        
+    //     if (data.GameSessionQueues && data.GameSessionQueues.length > 0) {
+    //         // for now just grab the first Queue
+    //         console.log("We have some queues");
+            
+    //         // if multiple queues, need to refactor this
+    //         let selectedGameSessionQueue;
+    //         data.GameSessionQueues.forEach(gameSessionQueue => {
+    //             if (gameSessionQueue.Name == TARGET_GAMELIFT_QUEUE_NAME) {
+    //                 selectedGameSessionQueue = gameSessionQueue;
+    //             }
+    //         });
+    //         return selectedGameSessionQueue;
+    //     }
+    //     else {
+    //         console.log("No queues available");
+    //         return [];
+    //     }
+    // }).catch(err => {
+    //     console.log(err);
+    //     return [];
+    // });
+}
+
+async function createGameSessionPlacement(targetQueueName, playerId) {
+    let createSessionInQueueRequest = {
+        GameSessionQueueName: targetQueueName,
+        PlacementId: uuidv4(), // generate unique placement id
+        MaximumPlayerSessionCount: MAX_PLAYER_COUNT,
+        DesiredPlayerSessions: [{
+            PlayerId: playerId   
+        }]
+    };
+    console.log("Calling startGameSessionPlacement...");
+
+    const command = new StartGameSessionPlacementCommand(createSessionInQueueRequest);
+    try {
+        const data = await gameLiftClient.send(command);
+        return data;
+    }
+    catch (error) {
+        console.log(error);
+        return [];
+    }
+    
+    // return await gameLiftClient.send(command).promise().then(data => {
+    //     // console.log(data);
+    //     return data;
+        
+    // }).catch(err => {
+    //     console.log(err);
+    //     return [];
+    // });
+}
+
+async function createPlayerSession(playerId, gameSessionId) {
+    let createPlayerSessionRequest = {
+      GameSessionId: gameSessionId,
+      PlayerId: playerId
+    };
+    
+    const command = new CreatePlayerSessionCommand(createPlayerSessionRequest);
+    try {
+        return await gameLiftClient.send(command)
+    }
+    catch (error) {
+        console.log(error);
+        return null;
+    }
+    
+    // return await gameLiftClient.send(command).promise().then(data => {
+    //     // console.log(data);
+    //     return data;
+    // }).catch(err => {
+    //     console.log(err);
+    //     return null;
+    // });
+}
+
+// where the lambda execution starts
+export const handler = async (event, context, callback) => {
+    console.log("Inside function...");
+    // console.log("environment: " + process.env.ENV);
+    // console.log(JSON.stringify(event, null, 2));
+
+    let message = JSON.parse(event.body);
+    console.log("Message received: %j", message);
+    
+    let responseMsg = {};
+
+    if (message && message.opCode) {
+
+        switch (message.opCode) {
+            case REQUEST_FIND_MATCH:
+                console.log("Request find match opCode hit");
+
+                let activeQueue = await getActiveQueue();
+                // console.log(activeQueue);
+
+                // The first destination is hardcoded here.  If you have more than one Alias or your setup is more complex, you’ll have to refactor. 
+                let gameSession = await searchGameSessions(activeQueue.Destinations[0].DestinationArn);
+
+                if (gameSession) { // Session found!
+                    console.log("We have a game session to join!");
+                    // console.log(gameSession);
+                    
+                    console.log("Creating player session...");
+                    let playerSession = await createPlayerSession(message.playerId, gameSession.GameSessionId);
+                    console.log("Player session created");
+                    // console.log(playerSession);
+                    
+                    responseMsg = playerSession.PlayerSession;
+                    responseMsg.PlayerSessionStatus = playerSession.PlayerSession.Status;
+                    responseMsg.GameSessionId = gameSession.GameSessionId;
+                    responseMsg.GameSessionStatus = gameSession.Status;
+                    
+                }
+                else { // Session not found, must create!
+                    console.log("No game sessions to join! " + activeQueue.Name);
+                    let gameSessionPlacement = await createGameSessionPlacement(activeQueue.Name, message.playerId);
+                    console.log("Game session placement request made");
+                    // console.log(gameSessionPlacement.GameSessionPlacement);
+                    responseMsg = gameSessionPlacement.GameSessionPlacement;
+                }
+
+                break;
+        }
+    }
+
+
+    return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(
+            responseMsg
+        )
+    });
+};
